361cb66a713e5ede731cd3e9fa38923fea317425
saikiran10496 <saikiran10496@gmail.com>
2025-04-14 17:11:00 +0530

    Update 0001_sample.sql

--- sql/migrations/0001_sample.sql
+++ sql/migrations/0001_sample.sql
@@ -1,10 +1,3 @@
+-- First script: Create database
 CREATE DATABASE TestDB;
-USE TestDB;
-
--- Create a sample table
-CREATE TABLE Employees (
-    ID INT PRIMARY KEY,
-    Name NVARCHAR(100),
-    Role NVARCHAR(50)
-);
-
+GO
